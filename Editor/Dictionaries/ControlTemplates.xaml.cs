0915cdba956a9f91efa88d687ca6c948ef73c335
JONGHUN\JH <dubi1052@gmail.com>
2023-06-16 17:50:10 +0900

    add default window style

--- Editor/Dictionaries/ControlTemplates.xaml.cs
+++ Editor/Dictionaries/ControlTemplates.xaml.cs
@@ -6,17 +6,20 @@ namespace Editor.Dictionaries
 	{
 		private void On_CloseBtn_Click(object sender, RoutedEventArgs e)
 		{
-
+			var window = (Window)((FrameworkElement)sender).TemplatedParent;
+			window.Close();
 		}
 
 		private void On_RestoreBtn_Click(object sender, RoutedEventArgs e)
 		{
-
+			var window = (Window)((FrameworkElement)sender).TemplatedParent;
+			window.WindowState = window.WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
 		}
 
 		private void On_MinimizeBtn_Click(object sender, RoutedEventArgs e)
 		{
-
+			var window = (Window)((FrameworkElement)sender).TemplatedParent;
+			window.WindowState = WindowState.Minimized;
 		}
 	}
 }
