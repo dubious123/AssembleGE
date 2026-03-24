726fe36863e35e324a1a99b16ef700e1360263ce
unknown <dubi1052@gmail.com>
2026-03-25 03:52:52 +0900

    project cleanup

--- Editor/utilities/Timing.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-
-namespace editor::utilities::timing
-{
-	std::string now_str();
-}
--- Editor/utilities/Xml.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-#include "pugixml/pugixml.hpp"
-
-namespace editor::utilities::Xml
-{
-}	 // namespace editor::utilities::Xml
